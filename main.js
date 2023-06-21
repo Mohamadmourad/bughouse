c33f97034f1d8fbbb8c2842f3aecc2917019556f
Mohamad Mourad <M.Mouradis511@gmail.com>
2023-06-21 07:46:54 +0300

    version1.0

---
+++ main.js
@@ -0,0 +1,23 @@
+document.getElementById('newsNav').addEventListener("click",function fun1(){
+    window.location.href="#news";
+});
+
+document.getElementById('whatIsNav').addEventListener("click",function fun1(){
+    window.location.href="#whatIs";
+});
+
+document.getElementById('latestwinnerNav').addEventListener("click",function fun1(){
+    window.location.href="#winner";
+});
+
+document.getElementById('contactNav').addEventListener("click",function fun1(){
+    window.location.href="#contactInfo";
+});
+
+document.getElementById('instagram').addEventListener("click",function fun1(){
+    window.location.href="https://www.instagram.com/thebughousehub/";
+});
+
+document.getElementById('facebook').addEventListener("click",function fun1(){
+    window.location.href="https://www.instagram.com/thebughousehub/";
+});
